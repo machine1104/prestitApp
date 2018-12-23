f2213492e96d16adcc027435a1bba777c6279ee0
machine1104 <machine1104@gmail.com>
2018-12-23 14:51:16 +0000

    prova css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)    # Not the final implementation!
         if @user.save
-            redirect_to 
+            redirect_to root_path
         else
             flash.now[:danger] = "Errore"
             render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   resources :users
+  
+  resources :users do
+    resources :transactions
+  end
   resources :transactions
 end
