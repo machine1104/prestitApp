4b4ab29b75c9867c9d92add1a3d3f7ed53f28372
machine1104 <machine1104@gmail.com>
2018-12-24 11:29:56 +0000

    fine

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -18,16 +18,18 @@ class Transaction < ApplicationRecord
         user = User.find(self.user_id)
         user.total = user.total + self.amount
         user.save
+        
         @API = "629726812:AAHC5UyTBd6sd2rLE5qYF_FsAOSUS_Fwc7s"
         @CHANNEL = "-1001494343697"
         @LINK = "https://api.telegram.org/bot"+@API+"/sendMessage?chat_id="+@CHANNEL+"&text="
         
         m = "#Totale "+ User.sum(:total).to_s
+        m += "\n"
         User.all.each do |u|
           m += "\n#Prestiti#{u.name} #{u.total.to_s}€"
         end
         
-        m += "\n#{self.amount.to_s}€ #{self.description} #{User.find(self.user_id).name}"
+        m += "\n\n#{self.amount.to_s}€ #{self.description} - #{User.find(self.user_id).name}"
         url = URI.parse(URI.encode(@LINK+m))
         res = HTTP.get(url).to_s
     end
@@ -36,6 +38,21 @@ class Transaction < ApplicationRecord
         user = User.find(self.user_id)
         user.total = user.total - self.amount
         user.save
+        
+        @API = "629726812:AAHC5UyTBd6sd2rLE5qYF_FsAOSUS_Fwc7s"
+        @CHANNEL = "-1001494343697"
+        @LINK = "https://api.telegram.org/bot"+@API+"/sendMessage?chat_id="+@CHANNEL+"&text="
+        
+        m = "#Totale "+ User.sum(:total).to_s
+        m += "\n"
+        User.all.each do |u|
+          m += "\n#Prestiti#{u.name} #{u.total.to_s}€"
+        end
+        
+        m += "\n\nTransazione eliminata: #{self.amount.to_s}€ #{self.description} - #{User.find(self.user_id).name}"
+        url = URI.parse(URI.encode(@LINK+m))
+        res = HTTP.get(url).to_s
+        
     end
     
 end
