1a4e09baed0662c5393115564cc2445d010d638b
machine1104 <machine1104@gmail.com>
2018-12-24 11:18:40 +0000

    prova telegram

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -21,24 +21,21 @@ class Transaction < ApplicationRecord
         @API = "629726812:AAHC5UyTBd6sd2rLE5qYF_FsAOSUS_Fwc7s"
         @CHANNEL = "-1001494343697"
         @LINK = "https://api.telegram.org/bot"+@API+"/sendMessage?chat_id="+@CHANNEL+"&text="
-    
-        m = "#Totale "
-        myUrl    = "http://example.com"
-        m += "\n\n#{myUrl}"
+        
+        m = "#Totale "+ User.sum(:total)
+        User.all.each do |u|
+          m += "\n#Prestiti#{u.name} #{u.total}€"
+        end
+        
+        m += "\n#{self.amount}€ #{self.description} #{User.find(self.user_id).name}"
         url = URI.parse(URI.encode(@LINK+m))
-        HTTP.get(url).to_s
+        res = HTTP.get(url).to_s
     end
     
     def update_destroy
         user = User.find(self.user_id)
         user.total = user.total - self.amount
         user.save
-        url = URI.parse('http://www.example.com/index.html')
-        req = Net::HTTP::Get.new(url.to_s)
-        Net::HTTP.start(url.host, url.port) {|http|
-          http.request(req)
-        }
-        
     end
     
 end
